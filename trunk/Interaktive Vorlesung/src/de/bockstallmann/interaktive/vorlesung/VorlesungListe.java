807b82c63f872702fa1170402d91a6cd4e4910ad
jstallmann <jstallmann@3bb50ad6-a2c6-cab1-0692-da0ab9e629a8>
2012-10-27 14:36:50 +0000

    VOrbereitung für den PDF_Viewer

--- trunk/Interaktive Vorlesung/src/de/bockstallmann/interaktive/vorlesung/VorlesungListe.java
+++ trunk/Interaktive Vorlesung/src/de/bockstallmann/interaktive/vorlesung/VorlesungListe.java
@@ -2,16 +2,27 @@ package de.bockstallmann.interaktive.vorlesung;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.support.v4.app.NavUtils;
 
 public class VorlesungListe extends Activity {
+	private Button openPdfbtn;
+	private Button exitbtn;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vorlesung_liste);
+        openPdfbtn = (Button) findViewById(R.id.openPDF_btn);
+        openPdfbtn.setOnClickListener(openPDF);
+        
+        exitbtn = (Button) findViewById(R.id.exit_btn);
+        exitbtn.setOnClickListener(exitApp);
     }
 
     @Override
@@ -20,5 +31,20 @@ public class VorlesungListe extends Activity {
         return true;
     }
 
-    
+    private OnClickListener openPDF = new OnClickListener(){
+		@Override
+		public void onClick(View v) {
+			Intent myIntent = new Intent(VorlesungListe.this, PdfReader.class);
+			VorlesungListe.this.startActivity(myIntent);
+			
+		}
+    };
+    private OnClickListener exitApp = new OnClickListener(){
+  		@Override
+  		public void onClick(View v) {
+  			finish();
+  			
+  			
+  		}
+      };
 }
