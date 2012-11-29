4352f98873f900f2497581eec2f69b37d5a50491
edward.bock <edward.bock@3bb50ad6-a2c6-cab1-0692-da0ab9e629a8>
2012-11-29 11:18:07 +0000

    Vorlesung hinzufügen

--- Interaktive Vorlesung/src/de/bockstallmann/interaktive/vorlesung/activities/CoursePreview.java
+++ Interaktive Vorlesung/src/de/bockstallmann/interaktive/vorlesung/activities/CoursePreview.java
@@ -3,27 +3,23 @@ package de.bockstallmann.interaktive.vorlesung.activities;
 import org.json.JSONArray;
 import org.json.JSONException;
 
-import de.bockstallmann.interaktive.vorlesung.R;
-import de.bockstallmann.interaktive.vorlesung.R.layout;
-import de.bockstallmann.interaktive.vorlesung.R.menu;
-import de.bockstallmann.interaktive.vorlesung.support.Constants;
-import de.bockstallmann.interaktive.vorlesung.support.JSONLoader;
-import de.bockstallmann.interaktive.vorlesung.support.list.CoursesInfoJSONHandler;
-import android.os.Bundle;
-import android.os.Messenger;
-import android.app.Activity;
-import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.DialogInterface.OnClickListener;
+import android.os.Bundle;
+import android.os.Messenger;
 import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+import de.bockstallmann.interaktive.vorlesung.R;
+import de.bockstallmann.interaktive.vorlesung.support.Constants;
+import de.bockstallmann.interaktive.vorlesung.support.JSONLoader;
+import de.bockstallmann.interaktive.vorlesung.support.list.CoursesInfoJSONHandler;
 
 public class CoursePreview extends FragmentActivity {
 
@@ -31,6 +27,8 @@ public class CoursePreview extends FragmentActivity {
 	private JSONLoader jsonLoader;
 	private String courseInfo;
 	private String pw;
+	private Builder builder;
+	private EditText pw_input;
 
 	@Override
     public void onCreate(Bundle savedInstanceState) {
@@ -40,10 +38,11 @@ public class CoursePreview extends FragmentActivity {
         int id = getIntent().getExtras().getInt(Constants.EXTRA_COURSE_ID);
         String title = getIntent().getExtras().getString(Constants.EXTRA_COURSE_TITLE);
         pw = getIntent().getExtras().getString(Constants.EXTRA_COURSE_PW);
+        
+        ((TextView)findViewById(R.id.actionbar_title)).setText(title);
+        
         jsonLoader = new JSONLoader(new Messenger(new CoursesInfoJSONHandler(this)));
 		jsonLoader.startGetCoursesInfo(id);
-		
-		
     }
 
     @Override
@@ -56,42 +55,52 @@ public class CoursePreview extends FragmentActivity {
     public void setCourseInfo(JSONArray serverDaten){
 		try {
 			courseInfo = serverDaten.getJSONObject(0).getString("info");
-			
 			Log.d("Ausgabe",courseInfo);
 		} catch (JSONException e) {
-			// TODO Auto-generated catch block
 			Log.d("CoursesInfo -Problem", "courseInfo");
 		}
 		short_info.setText(courseInfo);
 	}
     
     public void btn_add_click(final View view){
-    	Toast.makeText(this, "Click", Toast.LENGTH_SHORT).show();
-    	 AlertDialog.Builder builder = new AlertDialog.Builder(this);
- 	    // Get the layout inflater
- 	    LayoutInflater inflater = this.getLayoutInflater();
+    	if(pw == ""){
+    		final Intent intent = new Intent();
+			setResult(RESULT_OK, intent);
+			finish();
+			return;
+    	}
+    	builder = new Builder(this);
+    	builder.setTitle(R.string.dialog_pw_title);
+		pw_input = new EditText(this);
+		pw_input.setText("");
+		pw_input.setHint(R.string.dialog_pw_hint);
+		builder.setView(pw_input);
+		builder.setPositiveButton(R.string.dialog_btn_ok, new OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
 
- 	    // Inflate and set the layout for the dialog
- 	    // Pass null as the parent view because its going in the dialog layout
-
- 	    builder.setView(inflater.inflate(R.layout.pw_check, null))
- 	    // Add action buttons
- 	           
- 	           .setNegativeButton("Abbrechen", new DialogInterface.OnClickListener() {
- 	               public void onClick(DialogInterface dialog, int id) {
- 	            	   dialog.cancel();
- 	               }
- 	           })
- 	           .setPositiveButton("Starten", new DialogInterface.OnClickListener() {
- 	               @Override
- 	               public void onClick(DialogInterface dialog, int id) {  
- 	            		   EditText ed = (EditText) findViewById(R.id.password);
- 	            		   if(!ed.getText().toString().contentEquals(pw)){
- 	            			   Toast.makeText(CoursePreview.this, "Falsches Passwort", Toast.LENGTH_LONG).show();
- 	            		   }else Toast.makeText(CoursePreview.this, "eingabe richtig", Toast.LENGTH_LONG).show();;
- 	               }
- 	           }); 
- 	    builder.setTitle("Die Vorlesung ist Passwortgeschützt!");
- 	    builder.create();
+				if(!pw_input.getText().toString().contentEquals(pw)){
+      			   Toast.makeText(CoursePreview.this, "Falsches Passwort", Toast.LENGTH_LONG).show();
+			   } else {
+				   final Intent intent = new Intent();
+					setResult(RESULT_OK, intent);
+					finish();
+			   }
+			}
+		});
+		builder.setNegativeButton(R.string.dialog_btn_cancel, null);
+		builder.show();
     }
+    /**
+	 * Clickmethode für die Actionbaricons
+	 * @param view
+	 */
+	public void actionbarClick(final View view){
+		switch (view.getId()) {
+			case R.id.actionbar_back:
+			case R.id.actionbar_logo:
+				finish();
+				break;
+		}
+	}
 }
