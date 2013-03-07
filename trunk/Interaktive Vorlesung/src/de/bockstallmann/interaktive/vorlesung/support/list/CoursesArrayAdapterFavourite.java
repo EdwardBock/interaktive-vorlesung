936a5ae86e2cd7d10e9a39b5ca2354f9af71b17c
edward.bock <edward.bock@3bb50ad6-a2c6-cab1-0692-da0ab9e629a8>
2013-03-07 12:34:35 +0000

    Favstern, CoursesList Activity anpassungen

--- trunk/Interaktive Vorlesung/src/de/bockstallmann/interaktive/vorlesung/support/list/CoursesArrayAdapterFavourite.java
+++ trunk/Interaktive Vorlesung/src/de/bockstallmann/interaktive/vorlesung/support/list/CoursesArrayAdapterFavourite.java
@@ -3,14 +3,13 @@ package de.bockstallmann.interaktive.vorlesung.support.list;
 import java.util.ArrayList;
 
 import android.content.Context;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
 import android.widget.TextView;
-import android.view.View.OnClickListener;
 import de.bockstallmann.interaktive.vorlesung.R;
 import de.bockstallmann.interaktive.vorlesung.model.Course;
 import de.bockstallmann.interaktive.vorlesung.support.SQLDataHandler;
