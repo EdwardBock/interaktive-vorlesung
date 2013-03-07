c5c79cd62c50c49fccb424a191d497d33e4f9ba5
edward.bock <edward.bock@3bb50ad6-a2c6-cab1-0692-da0ab9e629a8>
2013-03-07 12:34:35 +0000

    Favstern, CoursesList Activity anpassungen

--- Interaktive Vorlesung/src/de/bockstallmann/interaktive/vorlesung/activities/ListCourses.java
+++ Interaktive Vorlesung/src/de/bockstallmann/interaktive/vorlesung/activities/ListCourses.java
@@ -8,11 +8,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Messenger;
 import android.support.v4.app.FragmentActivity;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.Animation.AnimationListener;
+import android.view.animation.AnimationSet;
+import android.view.animation.AnimationUtils;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.ScaleAnimation;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.AdapterView.OnItemLongClickListener;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -30,6 +38,7 @@ public class ListCourses extends FragmentActivity implements OnItemClickListener
     private ListView list;
 	private CoursesArrayAdapterFavourite courseListAdapter;
 	private SQLDataHandler db;
+	private EditText et_search;
 
 	@Override
     public void onCreate(Bundle savedInstanceState) {
@@ -41,6 +50,7 @@ public class ListCourses extends FragmentActivity implements OnItemClickListener
         courseListAdapter = new CoursesArrayAdapterFavourite(this, R.layout.course_row, new ArrayList<Course>() );
         TextView tv = (TextView) findViewById(R.id.tx_listcourses_emptyList);
 		list.setEmptyView(tv);
+		et_search = (EditText)findViewById(R.id.et_search);
     }
 	@Override
 	protected void onResume() {
@@ -81,9 +91,76 @@ public class ListCourses extends FragmentActivity implements OnItemClickListener
 		Toast.makeText(this, "ID: "+view.getId(), Toast.LENGTH_SHORT).show();
 		switch (view.getId()) {
 			case R.id.actionbar_add:
-				startActivity(new Intent(this,SearchCourse.class));
+				//startActivity(new Intent(this,SearchCourse.class));
+				toogleViewSlide(et_search);
+				break;
+			case R.id.actionbar_qr:
+				Intent intent = new Intent("com.google.zxing.client.android.SCAN.private");
+				intent.putExtra("SCAN_MODE", "QR_CODE_MODE");
+				startActivityForResult(intent, 0);
 				break;
 		}
 		
 	}
+	private void toogleViewSlide(final View activateView) {
+		if (activateView.getVisibility() == View.GONE)
+	    {
+	        //fade in from top
+	        activateView.setVisibility(View.VISIBLE);
+	        final Animation fadeInFromTopAnimation = AnimationUtils.loadAnimation(this, R.anim.fade_in_from_top);
+	        activateView.startAnimation(fadeInFromTopAnimation);
+	    } else {
+	    	//fade outfrom bottom
+	        final Animation fadeInFromTopAnimation = AnimationUtils.loadAnimation(this, R.anim.fade_out_from_bottom);
+	        fadeInFromTopAnimation.setAnimationListener(new AnimationListener()
+            {
+                public void onAnimationStart(Animation anim){}
+                public void onAnimationEnd(Animation anim) { activateView.setVisibility(View.GONE); }
+                public void onAnimationRepeat(Animation anim) {}
+            });
+	        activateView.startAnimation(fadeInFromTopAnimation);
+	        
+	    }
+	}
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if(resultCode == RESULT_OK){
+			switch (requestCode) {
+			case Constants.RC_ADD_COURSE:
+				Toast.makeText(this, 
+						"OK! Zur Datenbank hinzufügen", 
+						Toast.LENGTH_SHORT).show();
+				
+				//SQLite Datenbank füllen
+				//db.addCourse(new Course(db_id,title,reader,semester,year));
+				break;
+			case 0:
+				String contents = data.getStringExtra("SCAN_RESULT");
+                String format = data.getStringExtra("SCAN_RESULT_FORMAT");
+                // Handle successful scan
+               Toast toast = Toast.makeText(this, ":"+contents+":", Toast.LENGTH_LONG);
+               //toast.setGravity(Gravity.TOP, 25, 400);
+               //toast.show();
+                //qrcode_read = true;
+               //searchCourses(contents);
+                break;
+			default:
+				break;
+			}
+		}
+		super.onActivityResult(requestCode, resultCode, data);
+	}
+	
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		if ((keyCode == KeyEvent.KEYCODE_BACK)) {
+			if(et_search.getVisibility() == View.VISIBLE){
+				toogleViewSlide(et_search);
+				return true;
+			}
+		}
+		return super.onKeyDown(keyCode, event);
+	}
+    
 }
