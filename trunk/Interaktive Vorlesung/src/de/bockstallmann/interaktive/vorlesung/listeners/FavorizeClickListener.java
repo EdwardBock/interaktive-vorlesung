7689a38d802b1704e6887cf48842e244d18e39a4
edward.bock <edward.bock@3bb50ad6-a2c6-cab1-0692-da0ab9e629a8>
2013-03-11 10:30:57 +0000

    Favorisieren implementiert

--- trunk/Interaktive Vorlesung/src/de/bockstallmann/interaktive/vorlesung/listeners/FavorizeClickListener.java
+++ trunk/Interaktive Vorlesung/src/de/bockstallmann/interaktive/vorlesung/listeners/FavorizeClickListener.java
@@ -2,12 +2,33 @@ package de.bockstallmann.interaktive.vorlesung.listeners;
 
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.ImageView;
+import de.bockstallmann.interaktive.vorlesung.R;
+import de.bockstallmann.interaktive.vorlesung.model.Course;
+import de.bockstallmann.interaktive.vorlesung.support.SQLDataHandler;
 
 public class FavorizeClickListener implements OnClickListener {
 
+	private SQLDataHandler dh;
+	private ImageView fav;
+	private Course courseObj;
+	public FavorizeClickListener(Course course, ImageView favicon,  SQLDataHandler datahelper) {
+		dh = datahelper;
+		fav = favicon;
+		courseObj = course;
+		if(dh.hasCourseId(course.getID()))	favicon.setImageResource(R.drawable.ic_star_on);
+	}
 	@Override
 	public void onClick(View v) {
-		
+		if(dh.hasCourseId(courseObj.getID())){
+			if(dh.deleteCourse(this.courseObj)){
+				this.fav.setImageResource(R.drawable.ic_star_off);
+			}
+		} else {
+			if(dh.addCourse(this.courseObj)){
+				this.fav.setImageResource(R.drawable.ic_star_on);
+			}
+		}
 	}
 
 }
