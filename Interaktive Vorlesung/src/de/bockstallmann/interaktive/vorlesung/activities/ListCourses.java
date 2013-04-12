ce4c7caff4c1dd523a59217059374e574744dee7
edward.bock <edward.bock@3bb50ad6-a2c6-cab1-0692-da0ab9e629a8>
2013-04-12 16:57:07 +0000

    Eine menge Punkte abgearbeitet

--- Interaktive Vorlesung/src/de/bockstallmann/interaktive/vorlesung/activities/ListCourses.java
+++ Interaktive Vorlesung/src/de/bockstallmann/interaktive/vorlesung/activities/ListCourses.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.View;
@@ -13,7 +14,6 @@ import android.widget.AdapterView.OnItemClickListener;
 import android.widget.AdapterView.OnItemLongClickListener;
 import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 import de.bockstallmann.interaktive.vorlesung.R;
@@ -23,61 +23,70 @@ import de.bockstallmann.interaktive.vorlesung.support.Constants;
 import de.bockstallmann.interaktive.vorlesung.support.SQLDataHandler;
 import de.bockstallmann.interaktive.vorlesung.support.list.CoursesArrayAdapter;
 
-public class ListCourses extends FragmentActivity implements OnItemClickListener, OnItemLongClickListener {
+public class ListCourses extends FragmentActivity implements
+		OnItemClickListener, OnItemLongClickListener {
 
-    private ListView list;
+	private ListView list;
 	private CoursesArrayAdapter courseListAdapter;
 	private SQLDataHandler db;
 	private EditText et_search;
 	private CourseListListener courseListListener;
 
 	@Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_list_courses);
-        
-        // Dynamische und interaktive Views heraussuchen
-        list = (ListView)findViewById(R.id.lv_courses);
-        TextView tv = (TextView) findViewById(R.id.tx_listcourses_emptyList);
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_list_courses);
+
+		// Dynamische und interaktive Views heraussuchen
+		list = (ListView) findViewById(R.id.lv_courses);
+		TextView tv = (TextView) findViewById(R.id.tx_listcourses_emptyList);
 		list.setEmptyView(tv);
-		et_search = (EditText)findViewById(R.id.et_search);
-		
-    }
+		et_search = (EditText) findViewById(R.id.et_search);
+
+	}
+
 	@Override
 	protected void onResume() {
 		super.onResume();
-		
-        
-        // Alle Datenbank Kurse anzeigen
-        db  = new SQLDataHandler(this);
-        courseListAdapter = new CoursesArrayAdapter(this, R.layout.course_row, db.getCourses(), db );
-        
-        // Adapter und Listener übergeben
- 		list.setAdapter(courseListAdapter);
+
+		// Alle Datenbank Kurse anzeigen
+		db = new SQLDataHandler(this);
+		courseListAdapter = new CoursesArrayAdapter(this, R.layout.course_row,
+				db.getCourses(), db);
+
+		// Adapter und Listener übergeben
+		list.setAdapter(courseListAdapter);
 		list.setOnItemClickListener(this);
 		list.setOnItemLongClickListener(this);
-		courseListListener = new CourseListListener(list, et_search, findViewById(R.id.rl_progressbar));
-        
+		courseListListener = new CourseListListener(list, et_search,
+				findViewById(R.id.rl_progressbar));
+
 	}
+
 	@Override
 	protected void onStop() {
 		courseListListener.destroyListener();
 		super.onStop();
 	}
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_list_courses, menu);
-        return true;
-    }
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_list_courses, menu);
+		return true;
+	}
+
 	@Override
 	public boolean onItemLongClick(AdapterView<?> arg0, View arg1, int arg2,
 			long arg3) {
-		Toast.makeText(this, "LongClick: Kontext action menü Favoriten", Toast.LENGTH_SHORT).show();
+		Toast.makeText(this, "LongClick: Kontext action menü Favoriten",
+				Toast.LENGTH_SHORT).show();
 		return false;
 	}
+
 	@Override
-	public void onItemClick(final AdapterView<?> parent, final View view, final int position, final long id) {
-		Course course = (Course)parent.getItemAtPosition(position);
+	public void onItemClick(final AdapterView<?> parent, final View view,
+			final int position, final long id) {
+		Course course = (Course) parent.getItemAtPosition(position);
 		Intent intent = new Intent(this, ListSessions.class);
 		intent.putExtra(Constants.EXTRA_COURSE_ID, course.getID());
 		intent.putExtra(Constants.EXTRA_COURSE_TITLE, course.getTitle());
@@ -85,87 +94,93 @@ public class ListCourses extends FragmentActivity implements OnItemClickListener
 		intent.putExtra(Constants.EXTRA_COURSE_PW, course.getPassword());
 		startActivity(intent);
 	}
+
 	/**
 	 * Clickmethode für die Actionbaricons
+	 * 
 	 * @param view
 	 */
-	public void actionbarClick(final View view){
+	public void actionbarClick(final View view) {
 		switch (view.getId()) {
-			case R.id.actionbar_add:
-				//startActivity(new Intent(this,SearchCourse.class));
-				toggleViewSlide(et_search);
-				break;
-			case R.id.actionbar_qr:
-				Intent intent = new Intent("com.google.zxing.client.android.SCAN.private");
-				intent.putExtra("SCAN_MODE", "QR_CODE_MODE");
-				startActivityForResult(intent, 0);
-				break;
+		case R.id.actionbar_add:
+			// startActivity(new Intent(this,SearchCourse.class));
+			toggleViewSlide(et_search);
+			break;
+		case R.id.actionbar_qr:
+			Intent intent = new Intent(
+					"com.google.zxing.client.android.SCAN.private");
+			intent.putExtra("SCAN_MODE", "QR_CODE_MODE");
+			startActivityForResult(intent, Constants.RC_QR_CODE);
+			break;
 		}
-		
+
 	}
+
 	private void toggleViewSlide(final View activateView) {
-		if (activateView.getVisibility() == View.GONE)
-	    {
-	        //fade in from top
-	        activateView.setVisibility(View.VISIBLE);
-	        //final Animation fadeInFromTopAnimation = AnimationUtils.loadAnimation(this, R.anim.fade_in_from_top);
-	        //activateView.startAnimation(fadeInFromTopAnimation);
-	        et_search.requestFocus();
-	        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-	        imm.showSoftInput(et_search, InputMethodManager.SHOW_IMPLICIT);
-	    } else {
-	    	//fade outfrom bottom
-	       /* final Animation fadeInFromTopAnimation = AnimationUtils.loadAnimation(this, R.anim.fade_out_from_bottom);
-	        fadeInFromTopAnimation.setAnimationListener(new AnimationListener()
-            {
-                public void onAnimationStart(Animation anim){}
-                public void onAnimationEnd(Animation anim) { activateView.setVisibility(View.GONE); }
-                public void onAnimationRepeat(Animation anim) {}
-            });
-	        activateView.startAnimation(fadeInFromTopAnimation);
-	        */
-	        activateView.setVisibility(View.GONE);
-	    }
+		if (activateView.getVisibility() == View.GONE) {
+			// fade in from top
+			activateView.setVisibility(View.VISIBLE);
+			// final Animation fadeInFromTopAnimation =
+			// AnimationUtils.loadAnimation(this, R.anim.fade_in_from_top);
+			// activateView.startAnimation(fadeInFromTopAnimation);
+			et_search.requestFocus();
+			InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+			imm.showSoftInput(et_search, InputMethodManager.SHOW_IMPLICIT);
+		} else {
+			// fade outfrom bottom
+			/*
+			 * final Animation fadeInFromTopAnimation =
+			 * AnimationUtils.loadAnimation(this, R.anim.fade_out_from_bottom);
+			 * fadeInFromTopAnimation.setAnimationListener(new
+			 * AnimationListener() { public void onAnimationStart(Animation
+			 * anim){} public void onAnimationEnd(Animation anim) {
+			 * activateView.setVisibility(View.GONE); } public void
+			 * onAnimationRepeat(Animation anim) {} });
+			 * activateView.startAnimation(fadeInFromTopAnimation);
+			 */
+			activateView.setVisibility(View.GONE);
+		}
 	}
-	
+
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-		if(resultCode == RESULT_OK){
+		if (resultCode == RESULT_OK) {
 			switch (requestCode) {
 			case Constants.RC_ADD_COURSE:
-				Toast.makeText(this, 
-						"OK! Zur Datenbank hinzufügen", 
+				Toast.makeText(this, "OK! Zur Datenbank hinzufügen",
 						Toast.LENGTH_SHORT).show();
-				
-				//SQLite Datenbank füllen
-				//db.addCourse(new Course(db_id,title,reader,semester,year));
+
+				// SQLite Datenbank füllen
+				// db.addCourse(new Course(db_id,title,reader,semester,year));
 				break;
 			case 0:
+				Intent intent = new Intent(this, Questions.class);
+
 				String contents = data.getStringExtra("SCAN_RESULT");
-                String format = data.getStringExtra("SCAN_RESULT_FORMAT");
-                // Handle successful scan
-               Toast.makeText(this, format+":"+contents+":", Toast.LENGTH_LONG).show();
-       			Intent	intent = new Intent(this, Questions.class);
-               if(contents.contains("|")){
-                   intent.putExtra(Constants.EXTRA_SESSION_ID, Integer.parseInt(contents.substring(contents.indexOf("|")+1)));
-                   startActivity(intent);
-               }
-               //toast.setGravity(Gravity.TOP, 25, 400);
-               //toast.show();
-                //qrcode_read = true;
-               //searchCourses(contents);
-                break;
+				String format = data.getStringExtra("SCAN_RESULT_FORMAT");
+				if (!contents.matches(Constants.QR_REGEX)) {
+					Toast.makeText(this, "Konnte keine Präsentation finden...",
+							Toast.LENGTH_SHORT).show();
+					return;
+				}
+				// TODO: PW abfragen und favorit speichern
+				int session_id = Integer.parseInt(contents.substring(contents.indexOf("|") + 1));
+				// TODO: ListSession starten und da Questions starten lassen
+				Log.d("ListCourses","Starte session "+session_id);
+				break;
 			default:
 				break;
 			}
+		} else {
+			Log.d("ListCourses", "ResultCode: "+resultCode);
 		}
 		super.onActivityResult(requestCode, resultCode, data);
 	}
-	
+
 	@Override
 	public boolean onKeyDown(int keyCode, KeyEvent event) {
 		if ((keyCode == KeyEvent.KEYCODE_BACK)) {
-			if(et_search.getVisibility() == View.VISIBLE){
+			if (et_search.getVisibility() == View.VISIBLE) {
 				et_search.setText("");
 				toggleViewSlide(et_search);
 				return true;
@@ -173,5 +188,5 @@ public class ListCourses extends FragmentActivity implements OnItemClickListener
 		}
 		return super.onKeyDown(keyCode, event);
 	}
-    
+
 }
--- Interaktive Vorlesung/src/de/bockstallmann/interaktive/vorlesung/support/Constants.java
+++ Interaktive Vorlesung/src/de/bockstallmann/interaktive/vorlesung/support/Constants.java
@@ -1,35 +1,35 @@
 package de.bockstallmann.interaktive.vorlesung.support;
 
 public interface Constants {
-	
+
 	public static final int RC_ADD_COURSE = 1;
-	
+	public static final int RC_QR_CODE = 2;
+
+	public static final String QR_REGEX = "https://play.google.com/store/apps/details?id=" +
+			"de.edwardbock#?t=W251bGwsMSwxLDIxMiwiZGUuZWR3YXJkYm9jayJd&session|\\d+";
+
 	/**
 	 * Extras für die Activity übergabe
 	 */
 	public static final String EXTRA_COURSE_ID = "course_id";
 	public static final String EXTRA_COURSE_TITLE = "course_title";
 	public static final String EXTRA_COURSE_READER = "course_reader";
-	public static final String EXTRA_COURSE_PW ="course_pw";
+	public static final String EXTRA_COURSE_PW = "course_pw";
 	public static final String EXTRA_COURSE_SEMESTER = "course_semester";
 	public static final String EXTRA_COURSE_YEAR = "course_year";
-	
-	
+
 	/**
 	 * Extras für die Sessionübergabe
 	 */
 	public static final String EXTRA_SESSION_ID = "session_id";
-	
-	
-	
+
 	/**
 	 * JSON Serverresponse Namen.
 	 */
 	public static final String JSON_STATUS = "status";
 	public static final String JSON_DATEN = "daten";
 	public static final String JSON_MESSAGE = "message";
-	
-	
+
 	/**
 	 * Constants für Messenger message.
 	 */
@@ -39,7 +39,7 @@ public interface Constants {
 	 * SQL-Lite Konstanten
 	 */
 	public static final String DB_NAME = "InterVorlesung";
-	
+
 	public static final String TABLE_COURSES = "courses";
 	public static final String TABLE_COURSE_ID = "_id";
 	public static final String TABLE_COURSE_TITLE = "title";
@@ -47,19 +47,32 @@ public interface Constants {
 	public static final String TABLE_COURSE_YEAR = "year";
 	public static final String TABLE_COURSE_SEMESTER = "semester";
 	public static final String TABLE_COURSE_PW = "pw";
-	public static final String DELETE_TABLE_COURSES = "DROP TABLE "+TABLE_COURSES;
-	public static final String CREATE_TABLE_COURSES = "CREATE TABLE IF NOT EXISTS " + TABLE_COURSES +
-			" ("+
-			TABLE_COURSE_ID+" INTEGER PRIMARY KEY," +
-			TABLE_COURSE_TITLE+" VARCHAR(100) NOT NULL, " +
-			TABLE_COURSE_READER+" VARCHAR(100)NOT NULL, " +
-			TABLE_COURSE_SEMESTER+" VARCHAR(10)NOT NULL, " +
-			TABLE_COURSE_YEAR+" VARCHAR(10)NOT NULL) "; /*+
-			TABLE_COURSE_USER_ID+" INTEGER," +
-			TABLE_COURSE_SEMESTER+" VARCHAR(10) NOT NULL," +
-			TABLE_COURSE_PW+" VARCHAR(40)"
-			+" )";*/
-	
+	public static final String DELETE_TABLE_COURSES = "DROP TABLE "
+			+ TABLE_COURSES;
+	public static final String CREATE_TABLE_COURSES = "CREATE TABLE IF NOT EXISTS "
+			+ TABLE_COURSES
+			+ " ("
+			+ TABLE_COURSE_ID
+			+ " INTEGER PRIMARY KEY,"
+			+ TABLE_COURSE_TITLE
+			+ " VARCHAR(100) NOT NULL, "
+			+ TABLE_COURSE_READER
+			+ " VARCHAR(100)NOT NULL, "
+			+ TABLE_COURSE_SEMESTER
+			+ " VARCHAR(10)NOT NULL, "
+			+ TABLE_COURSE_YEAR + " VARCHAR(10)NOT NULL) "; /*
+															 * +
+															 * TABLE_COURSE_USER_ID
+															 * +" INTEGER," +
+															 * TABLE_COURSE_SEMESTER
+															 * +
+															 * " VARCHAR(10) NOT NULL,"
+															 * +
+															 * TABLE_COURSE_PW+
+															 * " VARCHAR(40)"
+															 * +" )";
+															 */
+
 	public static final String TABLE_QUESTS = "questions";
 	public static final String TABLE_QUEST_ID = "_id";
 	public static final String TABLE_QUEST_QUESTION = "question";
@@ -68,16 +81,23 @@ public interface Constants {
 	public static final String TABLE_QUEST_B = "b";
 	public static final String TABLE_QUEST_C = "c";
 	public static final String TABLE_QUEST_D = "d";
-	public static final String DELETE_TABLE_QUESTS = "DROP TABLE "+TABLE_QUESTS;
-	public static final String CREATE_TABLE_QUESTS = "CREATE TABLE IF NOT EXISTS " + TABLE_QUESTS +
-			" ("+
-			TABLE_QUEST_ID+" INTEGER PRIMARY KEY," +
-			TABLE_QUEST_QUESTION+" TEXT NOT NULL, " +
-			TABLE_QUEST_ANSWER+" VARCHAR(200) NOT NULL, " +
-			TABLE_QUEST_A+" INTEGER, " +
-			TABLE_QUEST_B+" INTEGER, "+
-			TABLE_QUEST_C+" INTEGER," +
-			TABLE_QUEST_D+" INTEGER"
-			+" )";
+	public static final String DELETE_TABLE_QUESTS = "DROP TABLE "
+			+ TABLE_QUESTS;
+	public static final String CREATE_TABLE_QUESTS = "CREATE TABLE IF NOT EXISTS "
+			+ TABLE_QUESTS
+			+ " ("
+			+ TABLE_QUEST_ID
+			+ " INTEGER PRIMARY KEY,"
+			+ TABLE_QUEST_QUESTION
+			+ " TEXT NOT NULL, "
+			+ TABLE_QUEST_ANSWER
+			+ " VARCHAR(200) NOT NULL, "
+			+ TABLE_QUEST_A
+			+ " INTEGER, "
+			+ TABLE_QUEST_B
+			+ " INTEGER, "
+			+ TABLE_QUEST_C
+			+ " INTEGER,"
+			+ TABLE_QUEST_D + " INTEGER" + " )";
 
 }
